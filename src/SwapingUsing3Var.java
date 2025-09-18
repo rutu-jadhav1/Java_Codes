de3c8def6a386df2b450ec3b9afca9f731a50d8e
rutu-jadhav1 <0103rutujajadhav@gmail.com>
2025-09-18 22:52:25 +0530

    swaping operation of 3 var

---
+++ src/SwapingUsing3Var.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class SwapingUsing3Var {
+    public static void main(String [] args){
+        System.out.print("Enter the first number : ");
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        System.out.print("Enter the second number :");
+        int b = sc.nextInt();
+
+        System.out.println("Values before swapping are a = " + a + " & b = " + b);
+
+        int c = a;
+        a = b ;
+        b = c;
+
+
+        System.out.println("Values after swapping are a = " + a + " & b = " + b);
+
+    }
+}
