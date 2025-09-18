42987c0f1daf1eb28cd438ef550d2609f37789a0
rutu-jadhav1 <0103rutujajadhav@gmail.com>
2025-09-18 22:34:32 +0530

    Arithmatic all operations

--- src/Arithmatic_Operations.java
+++ src/Arithmatic_Operations.java
@@ -10,6 +10,16 @@ public class Arithmatic_Operations {
         int num2 = sc.nextInt();
 
         int add = num1 + num2;
+        int sub = num1 - num2;
+        int mul = num1 * num2;
+        int div = num1 / num2;
+        int mod = num1 % num2;
+
         System.out.println("Addition :" +add);
+        System.out.println("Substraction :" +sub);
+        System.out.println("Multiplication :" +mul);
+        System.out.println("Division : " +div);
+        System.out.println("Remainder :" +mod);
+
     }
 }
